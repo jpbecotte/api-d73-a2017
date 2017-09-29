5a49b915e12d63c8da3b2c1f2fdbe9556d78e94c
Jean-Pierre Bécotte <jp.becotte@cmaisonneuve.qc.ca>
2017-09-29 11:19:42 -0400

    clients service

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const products = require('./products/products.service.js');
 const suppliers = require('./suppliers/suppliers.service.js');
+const clients = require('./clients/clients.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(products);
   app.configure(suppliers);
+  app.configure(clients);
 };
